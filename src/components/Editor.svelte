d75abca18714e970ec85ad9cf518e06284a74b14
Albert Abdulmanov <albert.abdu@gmail.com>
2021-04-24 21:04:52 +0500

    setup dynamic importing of huge monaco package

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,37 +1,14 @@
 <script lang="ts">
   import { onMount } from 'svelte'
+  import { editor } from 'monaco-editor'
+  import { getWorker } from '../lib/monaco'
 
-  import * as monaco from 'monaco-editor'
-  import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-  import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-  import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-  import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-  import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
-
-  window.MonacoEnvironment = {
-    getWorker(_workerId: string, label: string) {
-      if (label === 'json') {
-        return new jsonWorker()
-      }
-      if (label === 'css' || label === 'scss' || label === 'less') {
-        return new cssWorker()
-      }
-      if (label === 'html' || label === 'handlebars' || label === 'razor') {
-        return new htmlWorker()
-      }
-      if (label === 'typescript' || label === 'javascript') {
-        return new tsWorker()
-      }
-      return new editorWorker()
-    },
-  }
+  window.MonacoEnvironment = { getWorker }
 
   onMount(() => {
-    monaco.editor.create(document.getElementById('monaco'), {
-      value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
-        '\n'
-      ),
-      language: 'javascript',
+    editor.create(document.getElementById('monaco'), {
+      value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+      language: 'typescript',
     })
   })
 </script>
--- src/pages/Project.svelte
+++ src/pages/Project.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+  import { Circle2 as Spinner } from 'svelte-loading-spinners'
+
   import NotFound from './NotFound.svelte'
-  import Editor from '../components/Editor.svelte'
 
   import { projects } from '../lib/projects'
   import type { names } from '../lib/projects'
@@ -11,18 +12,54 @@
 </script>
 
 {#if project}
-  <Editor />
-  <iframe
-    src="/projects/{name}/{project.isBundled ? 'dist/' : ''}index.html"
-    frameborder="0"
-    class="project-iframe"
-    title={project.label}
-  />
+  <div class="project-page">
+    <h1>{project.label}</h1>
+    <div class="playground">
+      <div class="container">
+        {#await import('../components/Editor.svelte')}
+          <Spinner />
+        {:then { default: Editor }}
+          <svelte:component this={Editor} />
+        {:catch error}
+          <p>Something went wrong: {error.message}</p>
+        {/await}
+      </div>
+
+      <div class="container">
+        <iframe
+          src="/projects/{name}/{project.isBundled ? 'dist/' : ''}index.html"
+          frameborder="0"
+          class="project-iframe"
+          title={project.label}
+        />
+      </div>
+    </div>
+  </div>
 {:else}
   <NotFound />
 {/if}
 
 <style>
+  .project-page {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  h1 {
+    margin-bottom: 2rem;
+  }
+
+  .playground {
+    display: flex;
+    flex-grow: 1;
+  }
+
+  .container {
+    flex: 1 0 50%;
+  }
+
   .project-iframe {
     width: 100%;
     height: 100%;
